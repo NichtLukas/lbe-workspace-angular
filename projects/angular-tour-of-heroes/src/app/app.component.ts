34ca5da9a818489867f99fa856b65f5a85bee1f1
Lukas Bode <lukas.bode@geutebrueck.com>
2022-10-14 09:12:03 +0200

    feat: create angular-tour-of-heroes app

--- projects/angular-tour-of-heroes/src/app/app.component.ts
+++ projects/angular-tour-of-heroes/src/app/app.component.ts
@@ -6,9 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Tour of Heroes';
-
-  constructor() {
-    console.log('test')
-  }
+  title = 'angular-tour-of-heroes';
 }
