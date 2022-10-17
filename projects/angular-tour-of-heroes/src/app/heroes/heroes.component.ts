56ddb7ede30f282ca48f408ea8e3efe2236dc7d4
Lukas Bode <lukas.bode@geutebrueck.com>
2022-10-17 08:35:52 +0200

    feat: Select hero in list and update style

--- projects/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ projects/angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -5,19 +5,20 @@ import { Hero } from '../hero';
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: ['./heroes.component.scss']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
 
   heroes = HEROES;
+  selectedHero?: Hero;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    } 
+  
 
 }
